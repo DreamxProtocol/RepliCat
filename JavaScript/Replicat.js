a0fe0d045624145be705e9cba4a8a9f9a262ea7d
Marcelino <93845376+DreamxProtocol@users.noreply.github.com>
2021-12-17 12:00:33 -0800

    Initial commit

---
+++ JavaScript/Replicat.js
@@ -0,0 +1,13 @@
+function sale(){
+
+	document.getElementById("formz").style.display = "block";
+	document.getElementById("thex").style.display = "block"
+
+}
+
+function off(){
+
+	document.getElementById("formz").style.display = "none";
+	document.getElementById("thex").style.display = "none"
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RepliCat
+ My first landing page, inspired by Blade Runner (1982). 
